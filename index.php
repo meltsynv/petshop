1e310e73fca882c33dcc60fa7efa19a9807fc569
NadineBeka <nadine.beka@stud.hs-ruhrwest.de>
2019-05-15 21:09:14 +0200

    Kacheln verschoben und Überschrift geändert.

--- index.php
+++ index.php
@@ -49,33 +49,34 @@
 
   </div>
 </nav>
-<div class="d-md-flex justify-content-around w-100 my-md-3 pl-md-3">
-  <div class="bg-dark mr-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center text-white overflow-hidden" id="dog">
-    <div class="my-3 py-3">
-      <h2 class="display-5">Another headline</h2>
-      <img src="img/dog.jpg" alt="dog" height="300px" width="400px">
-      <p class="lead">And an even wittier subheading.</p>
+  <div class="row">
+    <div class="d-md-flex justify-content-around w-100 my-md-3 pl-md-3">
+      <div class="bg-dark mr-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center text-white overflow-hidden" id="dog">
+        <div class="my-3 py-3">
+          <h2 class="display-5">Dog environment</h2>
+          <img src="img/dog.jpg" alt="dog" height="300px" width="400px">
+          <p class="lead">And an even wittier subheading.</p>
+        </div>
+      </div>
+
+    <div class="bg-dark mr-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center text-white overflow-hidden" id="dog">
+        <div class="my-3 py-3">
+            <h2 class="display-5">Cat environment</h2>
+            <img src="img/cat.jpg" alt="cat" height="300px" width="400px">
+            <p class="lead">And an even wittier subheading.</p>
+        </div>
+    </div>
     </div>
   </div>
-
-  <div class="d-md-flex justify-content-around w-100 my-md-3 pl-md-3">
-    <div class="bg-dark mr-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center text-white overflow-hidden" id="dog">
-      <div class="my-3 py-3">
-        <h2 class="display-5">Another headline</h2>
-        <img src="img/cat.jpg" alt="cat" height="300px" width="400px">
-        <p class="lead">And an even wittier subheading.</p>
-      </div>
+    <div class="d-md-flex justify-content-around w-100 my-md-3 pl-md-3">
+        <div class="bg-dark mr-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center text-white overflow-hidden" id="dog">
+            <div class="my-3 py-3">
+                <h2 class="display-5">Rabbit environment</h2>
+                <img src="img/rabbit.jpg" alt="rabbit" height="300px" width="400px">
+                <p class="lead">And an even wittier subheading.</p>
+            </div>
+        </div>
     </div>
-
-  <div class="d-md-flex justify-content-around w-100 my-md-3 pl-md-3">
-    <div class="bg-dark mr-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center text-white overflow-hidden" id="dog">
-      <div class="my-3 py-3">
-        <h2 class="display-5">Another headline</h2>
-        <img src="img/rabbit.jpg" alt="rabbit" height="300px" width="400px">
-        <p class="lead">And an even wittier subheading.</p>
-      </div>
-    </div>
-  </div>
 
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
