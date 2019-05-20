fbaf053a312f9e2c49ab2255856cf74e89e2c096
NadineBeka <nadine.beka@stud.hs-ruhrwest.de>
2019-05-20 19:56:12 +0200

    Produktansicht angefangen und im Index Sidenav eingefügt. Versucht das Bild über der Menüleiste in der Höhe anzupassen.

--- index.php
+++ index.php
@@ -15,7 +15,7 @@
   <div class="jumbotron jumbotron-fluid">
     <div class="container">
       <h1 class="display-4">Petshop</h1>
-      <img  src="img/catAndDog.jpg" width="100%" alt="catAndDog">
+      <img  src="img/friends.jpg" width="100%" align-content="stretch" alt="friends">
     </div>
   </div>
 
@@ -37,7 +37,7 @@
         </form>
       </li>
         <li class="nav-item">
-        <a class="nav-link" href="#">Profil</a>
+        <a class="nav-link" href="index.php">Profil</a>
       </li>
       <li class="nav-item">
         <a class="nav-link" href="#">Cart</a>
@@ -46,9 +46,56 @@
         <a class="nav-link disabled" href="#">Disabled</a>
       </li>
     </ul>
-
   </div>
 </nav>
+
+
+  <div class="container-fluid">
+      <div class="row">
+          <nav class="col-md-2 d-none d-md-block bg-light sidebar">
+              <div class="sidebar-sticky">
+                  <ul class="nav flex-column">
+                      <li class="nav-item">
+                          <a class="nav-link active" href="#">
+                              <span data-feather="home"></span>
+                              Dashboard <span class="sr-only">(current)</span>
+                          </a>
+                      </li>
+                      <li class="nav-item">
+                          <a class="nav-link" href="#">
+                              <span data-feather="file"></span>
+                              Orders
+                          </a>
+                      </li>
+                      <li class="nav-item">
+                          <a class="nav-link" href="#">
+                              <span data-feather="shopping-cart"></span>
+                              Products
+                          </a>
+                      </li>
+                      <li class="nav-item">
+                          <a class="nav-link" href="#">
+                              <span data-feather="users"></span>
+                              Customers
+                          </a>
+                      </li>
+                      <li class="nav-item">
+                          <a class="nav-link" href="#">
+                              <span data-feather="bar-chart-2"></span>
+                              Reports
+                          </a>
+                      </li>
+                      <li class="nav-item">
+                          <a class="nav-link" href="#">
+                              <span data-feather="layers"></span>
+                              Integrations
+                          </a>
+                      </li>
+                  </ul>
+              </div>
+          </nav>
+
+
   <div class="row">
     <div class="d-md-flex justify-content-around w-100 my-md-3 pl-md-3">
       <div class="bg-dark mr-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center text-white overflow-hidden" id="dog">
