f1d0964a42bd6ed8bae745487ef09fa5d7493f97
lisawolff <lisa.wolff@stud.hs-ruhrwest.de>
2019-06-07 14:43:58 +0200

    produktübersicht

---
+++ produktübersichtKaninchen.php
@@ -0,0 +1,122 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" href="..\css\style_index.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+    <title>Webshop</title>
+</head>
+<body>
+
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <a class="navbar-brand" href="#">Navbar</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <span class="navbar-nav">
+      <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+    </span>
+
+    <div><h2>Rabbit environment</h2></div>//in die mitte verschieben
+
+
+    <div class="collapse navbar-collapse d-flex flex-row-reverse" id="navbarNav">
+        <ul class="navbar-nav">
+            <li>
+                <form class="form-inline my-2 my-lg-0">
+                    <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+                    <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+                </form>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="Produktansicht">Profil</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Cart</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link disabled" href="#">Disabled</a>
+            </li>
+        </ul>
+    </div>
+</nav>
+
+
+<img src="C:\xampp\htdocs\petshop\img\hundeleine1.jpg " alt="Hundeleine1" width="300" height="240"/>
+<div class="container">
+    <div class="row">
+        <div class="col-sm-1 col-md-1 col-lg-2 col-xl-2"></div>
+        <div class="col-sm-11 col-md-11 col-lg-10 col-xl-10">
+            <h1 class="firstOnSide">Hasen-Zubehör</h1>
+            <table class="Hasen-Artikel" border="" cellspacing="10" cellpadding="10">
+                <tr>
+                    <td><img src="C:\xampp\htdocs\petshop\img\hundeleine1.jpg " alt="Hundeleine1" width="300" height="240"/> </td>
+                    <td>Leine1<br>5.99 €</td>
+                    <td>Verfügbar:<br>Ja / Nein</td>
+                    <td><button class="btn"><i class="fa fa-shopping-cart"></i></button> </td>
+                    <td>Bwertunngen </td>
+                </tr>
+                <tr>
+                    <td>Bild </td>
+                    <td>Leine2<br>5.99 €</td>
+                    <td>Verfügbar:<br>Ja / Nein</td>
+                    <td><button class="btn"><i class="fa fa-shopping-cart"></i></button> </td>
+                    <td>Bwertunngen </td>
+                </tr>
+                <tr>
+                    <td>Bild </td>
+                    <td>Geschirr<br>5.99 €</td>
+                    <td>Verfügbar:<br>Ja / Nein</td>
+                    <td><button class="btn"><i class="fa fa-shopping-cart"></i></button> </td>
+                    <td>Bwertunngen </td>
+                </tr>
+                <tr>
+                    <td>Bild </td>
+                    <td>Napf<br>5.99 €</td>
+                    <td>Verfügbar:<br>Ja / Nein</td>
+                    <td><button class="btn"><i class="fa fa-shopping-cart"></i></button> </td>
+                    <td>Bwertunngen </td>
+                </tr>
+                <tr>
+                    <td>Bild </td>
+                    <td>Leckerchen<br>5.99 €</td>
+                    <td>Verfügbar:<br>Ja / Nein</td>
+                    <td><button class="btn"><i class="fa fa-shopping-cart"></i></button> </td>
+                    <td>Bwertunngen </td>
+                </tr>
+                <tr>
+                    <td>Bild </td>
+                    <td>SPielzeug<br>5.99 €</td>
+                    <td>Verfügbar:<br>Ja / Nein</td>
+                    <td><button class="btn"><i class="fa fa-shopping-cart"></i></button> </td>
+                    <td>Bwertunngen </td>
+                </tr>
+                <tr>
+                    <td>Bild </td>
+                    <td>Halsband<br>5.99 €</td>
+                    <td>Verfügbar:<br>Ja / Nein</td>
+                    <td><button class="btn"><i class="fa fa-shopping-cart"></i></button> </td>
+                    <td>Bwertunngen </td>
+                </tr>
+            </table>
+        </div>
+    </div>
+</div>-
+
+
+
+
+
+<!-- Optional JavaScript -->
+<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+</body>
+</html>
