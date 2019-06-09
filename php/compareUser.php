556edbb4f8487edc3af094872149f5b3cae1b02f
NadineBeka <nadine.beka@stud.hs-ruhrwest.de>
2019-06-09 13:56:33 +0200

    Nutzer können sich anmelden

--- php/compareUser.php
+++ php/compareUser.php
@@ -5,16 +5,19 @@ if (isset($_POST['loginBtn'])){
     $email = $_POST['Email'];
     $password = $_POST['Passwort'];
 
-    $query  = "SELECT Email, Passwort FROM user";
+    $query  = "SELECT * FROM user";
     $result = mysqli_query($con, $query);
-    while($row = mysqli_fetch_assoc($result)) {
-       if ($password == $row[1] && $row[0]==$email){
+    while($row = mysqli_fetch_array($result)) {
+        if ($password == $row[4] && $row[2]==$email){
             header("location:../index.php");
+            echo "Super";
         }
         else{
-            echo "fail";        }
+            echo "Bitte geben Sie die korrekten Zugangsdaten ein.";
+        }
 
     }
+
 
 }
 else{
