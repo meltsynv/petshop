e6308b9383c226964c74020940f814105d1d5c30
NadineBeka <nadine.beka@stud.hs-ruhrwest.de>
2019-06-15 21:45:25 +0200

    Nutzersuche funktioniert, wird aber noch nicht vernünftig ausgegeben.

--- adminIndex.php
+++ adminIndex.php
@@ -1,20 +1,34 @@
-<?php include 'inc/head.php'?>
-<form class="form-inline my-2 my-lg-0">
-    <input class="form-control mr-sm-2" type="search" name="searchLabel" placeholder="Search" id="search" aria-label="Search">
-    <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-</form>
+<?php include "inc/head.php"?>
+<div>
+    <form class="form-inline my-2 my-lg-0" method="post" action="php/searchUser.php">
+    <input class="form-control mr-sm-2" type="search" placeholder="Search" name="searchLabel" aria-label="Search">
+    <button class="btn btn-outline-success my-2 my-sm-0" name="searchBtn" type="submit">Search</button>
+    </form>
+</div>
 <div class="container">
-<button type="button" name="btnNutzer" onclick="parent.location='adminNutzerliste.php'">Nutzer</button>
-<button type="button" name="btnProdukte" onclick="parent.location='adminProduktübersicht.php'">Produkte</button>
+    <button type="button" name="btnNutzer" onclick="parent.location='adminNutzerliste.php'">Nutzer</button>
+    <button type="button" name="btnProdukte" onclick="parent.location='adminProduktübersicht.php'">Produkte</button>
 </div>
 
-<script>
-    $(function() {
+    <script>
+        // Example starter JavaScript for disabling form submissions if there are invalid fields
+        (function() {
+            'use strict';
+            window.addEventListener('load', function() {
+                // Fetch all the forms we want to apply custom Bootstrap validation styles to
+                var forms = document.getElementsByClassName('needs-validation');
+                // Loop over them and prevent submission
+                var validation = Array.prototype.filter.call(forms, function(form) {
+                    form.addEventListener('submit', function(event) {
+                        if (form.checkValidity() === false) {
+                            event.preventDefault();
+                            event.stopPropagation();
+                        }
+                        form.classList.add('was-validated');
+                    }, false);
+                });
+            }, false);
+        })();
+    </script>
 
-        $( "#search" ).autocomplete(
-            {
-                source:'searchUser.php',
-            })
-
-    });
-</script>
+<?php include "inc/bottom.php";
--- php/searchUser.php
+++ php/searchUser.php
@@ -1,12 +1,39 @@
 <?php
-$req = "SELECT Vorname FROM user WHERE Vorname LIKE '%".$_REQUEST['term']."%' ";
-$query = mysqli_query($con,$req);
+include "../inc/config.php";
+if (isset($_POST['searchBtn'])){
+    $input = $_POST['searchLabel'];
+    $query = "SELECT * FROM user WHERE Vorname LIKE '%$input%'";
+    $result  = mysqli_query($con, $query);
+    ?>
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <table class="table table-striped">
+                    <tr>
+                        <th scope="col">Vorname</th>
+                        <th scope="col">Nachname</th>
+                        <th scope="col">Email</th>
+                        <th scope="col"></th>
+                    </tr>
+                    <?php
+                    while ($row = mysqli_fetch_assoc($result)) {
+                        $vorname = $row['Vorname'];
+                        $nachname = $row['Nachname'];
+                        $email = $row['Email'];
+                        ?>
+                        <tr>
+                            <td><?php echo $vorname ?></td>
+                            <td><?php echo $nachname ?></td>
+                            <td><?php echo $email ?></td>
+                            <td> <button type="button" id="bearbeiten">bearbeiten</button></td>
+                        </tr>
+                    <?php }}
+                else{
+                    echo "fail";
+                }?>
+                </table>
+            </div>
+         </div>
+    </div>
 
-if (isset($_POST['search'])){
-    $Name = $_POST['search'];
-}
-while($row = mysqli_fetch_array($query)) {
-    $results[] = array('label' => $row['Vorname']);
-}
 
-echo json_encode($results);
