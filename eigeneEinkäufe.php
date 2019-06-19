55b29c20e3e03410975ff9a5082d132156384004
lisawolff <lisa.wolff@stud.hs-ruhrwest.de>
2019-06-19 20:21:49 +0200

    Bewertung markieren geht bisher nur über die ID es Fehlt aber noch die bedingung des USers

--- eigeneEinkäufe.php
+++ eigeneEinkäufe.php
@@ -8,18 +8,8 @@ session_start() ;
 if (isset($_SESSION['Username'])) {
     $user = $_SESSION['Username'];
 }
-//DB-Abfrage nach gekauften Artikel von aktivem Nutzer
-$query3 = "SELECT * FROM kaeufe INNER JOIN produkte WHERE Username = '$user'";
-$result3 = mysqli_query($con, $query3);
-
-if ($row = mysqli_fetch_assoc($result3)) {
-    $bild = $row['Bild'];
-    $artikelName = $row['Name'];
-    $datum = $row['Datum'];
-    $price= $row['Preis'];
 
     ?>
-
 
 <div class="row justify-content-center">
         <div class="col-10">
@@ -32,19 +22,21 @@ if ($row = mysqli_fetch_assoc($result3)) {
                     <th scope="col">Preis</th>
                 </tr>
                 <?php
-                //Ausgabe der Ergebnisse
-                while ($row = mysqli_fetch_assoc($result3)) {
-                    $username = $row['Username'];
-                    $vorname = $row['Vorname'];
-                    $nachname = $row['Nachname'];
-                    $email = $row['Email'];
+                //Ausgabe der Ergebnisse, welche der User gekauft hat
+                $query4 = "SELECT * FROM kaeufe INNER JOIN produkte WHERE Username = '$user'";
+                $result4 = mysqli_query($con, $query4);
+                while ($row = mysqli_fetch_assoc($result4)) {
+                    $bild = $row['Bild'];
+                    $datum = $row['Datum'];
+                    $artikelName = $row['Name'];
+                    $price = $row['Preis'];
                     ?>
                 <tr>
                     <td><img src="<?php echo $bild ?>" width="200"></td>
                     <td><?php echo $datum ?></td>
                     <td><?php echo $artikelName ?></td>
                     <td><?php echo $price ?></td>
-                </tr><?php }}?>
+                </tr><?php }?>
             </table>
         </div>
     </div>
