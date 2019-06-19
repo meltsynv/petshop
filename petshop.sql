a5bf70aba6f2a6169b806f6b59713cf37b7ced5c
NadineBeka <nadine.beka@stud.hs-ruhrwest.de>
2019-06-19 19:09:09 +0200

    aktuelle DB

--- petshop.sql
+++ petshop.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Erstellungszeit: 17. Jun 2019 um 21:24
+-- Erstellungszeit: 19. Jun 2019 um 19:08
 -- Server-Version: 10.1.37-MariaDB
 -- PHP-Version: 7.3.0
 
@@ -41,11 +41,22 @@ CREATE TABLE `bewertungen` (
 --
 
 INSERT INTO `bewertungen` (`ID`, `Kommentar`, `Note`, `Datum`, `User`) VALUES
-(1, 'Super Leine!', 4, '2019-06-16 12:09:08', 'Unbekannt'),
 (1, 'Schönes Blau!', 3, '2019-06-16 13:03:15', 'Unbekannt'),
-(1, 'Ganz toll.', 4, '2019-06-17 13:17:50', 'Unbekannt'),
 (1, 'Nicht ueberzeugt', 1, '2019-06-17 13:24:57', 'Unbekannt'),
-(2, 'Toll...', 6, '2019-06-17 13:37:58', 'Unbekannt');
+(1, 'Ganz toll!', 2, '2019-06-18 14:17:26', 'Unbekannt'),
+(3, 'Ganz toll.', 2, '2019-06-19 12:59:03', 'Nadine');
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `kaeufe`
+--
+
+CREATE TABLE `kaeufe` (
+  `ArtikelID` int(11) NOT NULL,
+  `Username` text COLLATE utf16_german2_ci NOT NULL,
+  `Datum` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf16 COLLATE=utf16_german2_ci;
 
 -- --------------------------------------------------------
 
@@ -67,8 +78,30 @@ CREATE TABLE `produkte` (
 --
 
 INSERT INTO `produkte` (`ID`, `Name`, `Preis`, `Stueck`, `Bild`, `Beschreibung`) VALUES
-(1, 'Leine', 15, 20, 'img/hundeleine1.jpg', 'Blaue Leine'),
-(2, 'Katzenleine', 13, 12, 'img/geschirrKatze.jpg', 'Rote Katzenleine. 75%Baumwolle');
+(1, 'Blaue Leine', 15, 20, 'img/hundeleine1.jpg', 'Helles Blau, mit einem Haken.'),
+(3, 'Knallrotes Katzengeschirr.', 10, 25, 'img/geschirrKatze.jpg', 'Knallrot. Groesse ist anpassbar.');
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `profil`
+--
+
+CREATE TABLE `profil` (
+  `Username` text COLLATE utf16_german2_ci NOT NULL,
+  `Datum` date NOT NULL,
+  `Kommentar` text COLLATE utf16_german2_ci NOT NULL,
+  `Note` double NOT NULL,
+  `Artikel` text COLLATE utf16_german2_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf16 COLLATE=utf16_german2_ci;
+
+--
+-- Daten für Tabelle `profil`
+--
+
+INSERT INTO `profil` (`Username`, `Datum`, `Kommentar`, `Note`, `Artikel`) VALUES
+('Nadine', '2019-06-18', 'Tolles Geschirr!', 2, '2'),
+('Nadine', '2019-06-19', 'Ganz toll.', 2, '3');
 
 -- --------------------------------------------------------
 
@@ -97,6 +130,12 @@ INSERT INTO `user` (`Vorname`, `Nachname`, `Email`, `Username`, `Passwort`) VALU
 --
 
 --
+-- Indizes für die Tabelle `bewertungen`
+--
+ALTER TABLE `bewertungen`
+  ADD KEY `Update` (`ID`);
+
+--
 -- Indizes für die Tabelle `produkte`
 --
 ALTER TABLE `produkte`
@@ -116,7 +155,17 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT für Tabelle `produkte`
 --
 ALTER TABLE `produkte`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- Constraints der exportierten Tabellen
+--
+
+--
+-- Constraints der Tabelle `bewertungen`
+--
+ALTER TABLE `bewertungen`
+  ADD CONSTRAINT `Update` FOREIGN KEY (`ID`) REFERENCES `produkte` (`ID`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
